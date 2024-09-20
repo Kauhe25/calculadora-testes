b32cd7eeb27e9284ee83e3626798edd299f9007a
kauhe <kauhe.silva@procempa.com.br>
2024-09-19 21:41:35 -0300

    criando a estrutura da calculadora

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const moduloCalculadora = require("./calculadora")
+//console.log(moduloCalculadora.soma)
+
+console.log(" ----------- Minha Calculadora ---------")
+const resultado = moduloCalculadora.soma(2,5)
+console.log(resultado)
+
+console.log(" ----------- Fim ---------")
